05ee4af39ba0783fe4197e397910db26fab2b2dc
Evan Moore <2emoore4@gmail.com>
2013-09-20 14:50:54 -0400

    added new test patterns: full white frame, full black frame, and two half and half frames which are opposites. added two methods to test_patterns, one which displays a sequence that alternates the two full frames, and one which displays a sequence that alternates the two half frames.

--- test_patterns.c
+++ test_patterns.c
@@ -4,7 +4,8 @@
 #include "error.h"
 #include "lcr_cmd.h"
 
-static void mSleep(unsigned long int mSeconds);
+static void flash_solid_color();
+static void flash_half_color();
 
 /* main() function */
 int main(int argc, char *argv[])
@@ -18,10 +19,14 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    printf("this doesn't do anything yet. press 'x' to quit.\n");
+    printf("press 'x' to quit, enter '1' to flash a solid frame, enter '2' to flash a half-filled-frame.\n");
 
-    while((ch=getchar()) != 'x') {
-
+    while((ch = getchar()) != 'x') {
+        if (ch == '1') {
+            flash_solid_color();
+        } else if (ch == '2') {
+            flash_half_color();
+        }
     }
 
     LCR_CMD_Close();
@@ -29,10 +34,80 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-/*Function is used to create delay in mSeconds */
-/* This is used in demo code to show code flow */
-static void mSleep(unsigned long int mSeconds)
-{
-    /*Add logic and call system function that will create mSeconds millisecond delay */
-    sleep((mSeconds/1000.0));
+static void flash_solid_color() {
+    LCR_PatternSeqSetting_t patSeqSet;
+
+    printf("\n\nrunning flash_solid_color demo for ten seconds\n\n");
+
+    LCR_CMD_SetDisplayMode((LCR_DisplayMode_t)(0x04));
+
+    printf("Selecting BIT_DEPTH = 1, NUM_PAT = 2, TRIGGER_TYPE = AUTO, AUTO_TRIG_PEIORD = 33334uSec, EXPOSURE = 33334uSec, LED = GREEN\n");
+    patSeqSet.BitDepth = 1;
+    patSeqSet.NumPatterns = 2;
+    patSeqSet.PatternType = PTN_TYPE_NORMAL;
+    patSeqSet.InputTriggerDelay = 0;
+    patSeqSet.InputTriggerType = TRIGGER_TYPE_AUTO;
+    patSeqSet.AutoTriggerPeriod = 8333;
+    patSeqSet.ExposureTime = 8333;
+    patSeqSet.LEDSelect = LED_GREEN;
+    patSeqSet.Repeat = 0;
+    LCR_CMD_SetPatternSeqSetting(&patSeqSet);
+
+    printf("Downloading pat0\n");
+    LCR_CMD_DefinePatternBMP(0,"./Images/test/white.bmp");
+
+    printf("Downloading pat1...\n");
+    LCR_CMD_DefinePatternBMP(1,"./Images/test/black.bmp");
+
+    printf("Starting Pattern Sequence...\n");
+    LCR_CMD_StartPatternSeq(1);
+
+    uint8 ch;
+    printf("enter 's' to stop sequence.\n");
+    while((ch = getchar()) != 's') {
+        // nothing yet.
+    }
+
+    LCR_CMD_StartPatternSeq(0); //Stop pattern sequence
+
+    LCR_CMD_SetDisplayMode((LCR_DisplayMode_t)(0x00)); //Setting back to static display mode
+}
+
+static void flash_half_color() {
+    LCR_PatternSeqSetting_t patSeqSet;
+
+    printf("\n\nrunning flash_half_color demo for ten seconds\n\n");
+
+    LCR_CMD_SetDisplayMode((LCR_DisplayMode_t)(0x04));
+
+    printf("Selecting BIT_DEPTH = 1, NUM_PAT = 2, TRIGGER_TYPE = AUTO, AUTO_TRIG_PEIORD = 33334uSec, EXPOSURE = 33334uSec, LED = GREEN\n");
+    patSeqSet.BitDepth = 1;
+    patSeqSet.NumPatterns = 2;
+    patSeqSet.PatternType = PTN_TYPE_NORMAL;
+    patSeqSet.InputTriggerDelay = 0;
+    patSeqSet.InputTriggerType = TRIGGER_TYPE_AUTO;
+    patSeqSet.AutoTriggerPeriod = 8333;
+    patSeqSet.ExposureTime = 8333;
+    patSeqSet.LEDSelect = LED_GREEN;
+    patSeqSet.Repeat = 0;
+    LCR_CMD_SetPatternSeqSetting(&patSeqSet);
+
+    printf("Downloading pat0\n");
+    LCR_CMD_DefinePatternBMP(0,"./Images/test/half1.bmp");
+
+    printf("Downloading pat1...\n");
+    LCR_CMD_DefinePatternBMP(1,"./Images/test/half2.bmp");
+
+    printf("Starting Pattern Sequence...\n");
+    LCR_CMD_StartPatternSeq(1);
+
+    uint8 ch;
+    printf("enter 's' to stop sequence.\n");
+    while((ch = getchar()) != 's') {
+        // nothing yet.
+    }
+
+    LCR_CMD_StartPatternSeq(0); //Stop pattern sequence
+
+    LCR_CMD_SetDisplayMode((LCR_DisplayMode_t)(0x00)); //Setting back to static display mode
 }
