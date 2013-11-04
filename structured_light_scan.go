128a8b5022d236432e687a3380344dedc0fd3f0e
Evan Moore <2emoore4@gmail.com>
2013-11-04 17:16:51 -0500

    still updating the go scanner. this commit adds calibration functionality.

--- structured_light_scan.go
+++ structured_light_scan.go
@@ -3,6 +3,7 @@ package main
 import "bufio"
 import "flag"
 import "fmt"
+import "image"
 import "image/png"
 import "io/ioutil"
 import "math"
@@ -17,7 +18,7 @@ var frame_count int = 96
 var image_size_x, image_size_y = 640, 480
 var sub_sampling_rate, expected_x_deviation = 8, 100
 
-func scan_dir(dir_name string) {
+func scan_dir(dir_name string, calibration_dir string) {
 	fmt.Println("Processing scans in directory: " + dir_name)
 	print_divider()
 
@@ -25,6 +26,11 @@ func scan_dir(dir_name string) {
 	if len(files) == 0 {
 		fmt.Println("Directory is empty, make sure there are scans in here.")
 	} else {
+		var cal_array [][]int = nil
+		if calibration_dir != "" {
+			cal_array = calibrate_from_dir(calibration_dir)
+		}
+
 		z_array := make([][]float64, image_size_x)
 		for i := range z_array {
 			z_array[i] = make([]float64, image_size_y)
@@ -45,7 +51,7 @@ func scan_dir(dir_name string) {
 		for i := 0; i < len(files); i++ {
 			filename := files[i]
 			if strings.Contains(filename.Name(), "png") {
-				scan_image_by_dev(dir_name + filename.Name(), scan_number, z_array)
+				scan_image_by_dev(dir_name + filename.Name(), scan_number, z_array, cal_array)
 				fmt.Println("scanning image " + filename.Name())
 				scan_number++
 			}
@@ -58,52 +64,108 @@ func scan_dir(dir_name string) {
 	}
 }
 
+func calibrate_from_dir(dir_name string) [][]int {
+	fmt.Println("Calibrating scanner with frames from directory: " + dir_name)
+	print_divider()
+
+	var files, _ = ioutil.ReadDir(dir_name)
+	if len(files) == 0 {
+		fmt.Println("Calibration directory is empty, make sure there are scans in here.")
+	} else {
+		cal_array := make([][]int, image_size_x)
+		for i := range cal_array {
+			cal_array[i] = make([]int, image_size_y)
+		}
+
+		for x := 0; x < image_size_x; x++ {
+			for y := 0; y < image_size_y; y++ {
+				cal_array[x][y] = -99999
+			}
+		}
+
+		fmt.Println("Scanning images and creating calibration array...")
+		print_divider()
+
+		scan_number := 0
+		for i := 0; i < len(files); i++ {
+			filename := files[i]
+			if strings.Contains(filename.Name(), "png") {
+				calibrate_with_image(dir_name + filename.Name(), scan_number, cal_array)
+				scan_number++
+			}
+		}
+
+		fmt.Println("Finished calibration.")
+		print_divider()
+
+		return cal_array
+	}
+
+	return nil
+}
+
+func find_projection_in_line(scan image.Image, y_pix int) int {
+	enter_white, exit_white := 0, 0
+
+	for x := 0; x < image_size_x; x++ {
+		color := scan.At(x, y_pix)
+		r, g, b, _ := color.RGBA()
+		if is_white(r, g, b) {
+			enter_white = x
+			break
+		}
+	}
+
+	for x := enter_white; x < image_size_x; x++ {
+		color := scan.At(x, y_pix)
+		r, g, b, _ := color.RGBA()
+		if !is_white(r, g, b) {
+			exit_white = x - 1
+			break
+		}
+	}
+
+	if enter_white != 0 && exit_white != 0 {
+		return int((enter_white + exit_white) / 2)
+	}
+
+	return -99999;
+}
+
+func calibrate_with_image(filename string, scan_number int, cal_array [][]int) {
+	fmt.Println("Opening file " + filename)
+
+	file, err := os.Open(filename)
+	if err != nil { panic(err) }
+	r := bufio.NewReader(file)
+	scan, err := png.Decode(r)
+
+	for y := 0; y < image_size_y; y++ {
+		line_location := find_projection_in_line(scan, y)
+		if line_location != -99999 {
+			cal_array[scan_number][y] = line_location
+		}
+	}
+}
+
 func scan_video(filename string) {
 	fmt.Println("going to scan video file: " + filename)
 }
 
-func scan_image(filename string, scan_number int, z_array [][]float64) {
+func scan_image_with_parallax(filename string, scan_number int, z_array [][]float64) {
 	fmt.Println("Opening file " + filename + ". scan number " + strconv.Itoa(scan_number))
 
 	file, err := os.Open(filename)
 	if err != nil { panic(err) }
 	r := bufio.NewReader(file)
-	scan_image, err := png.Decode(r)
+	scan, err := png.Decode(r)
 
 	start_time := time.Now()
 
-	previous_line_location := -1
 	for y := 0; y < image_size_y; y += sub_sampling_rate {
-		enter_white, exit_white := 0, 0
-		var min_x, max_x int
-
-		if previous_line_location == -1 || previous_line_location < expected_x_deviation || previous_line_location > (image_size_x - expected_x_deviation) {
-			min_x, max_x = 0, image_size_x
-		} else {
-			min_x, max_x = previous_line_location - expected_x_deviation, previous_line_location + expected_x_deviation
-		}
-
-		for x := min_x; x < max_x; x++ {
-			color := scan_image.At(x, y)
-			r, g, b, _ := color.RGBA()
-			if is_white(r, g, b) {
-				enter_white = x
-				break
-			}
-		}
-		for x := enter_white; x < max_x; x++ {
-			color := scan_image.At(x, y)
-			r, g, b, _ := color.RGBA()
-			if !is_white(r, g, b) {
-				exit_white = x - 1
-				break
-			}
-		}
-
-		if enter_white != 0 && exit_white != 0 {
-			mid_white := int((enter_white + exit_white) / 2)
-			previous_line_location = mid_white
-			z_array[mid_white][y] = z_triangulation(mid_white, y, scan_number)
+		line_location := find_projection_in_line(scan, y)
+		if line_location != -99999 {
+			z_array[line_location][y] = z_triangulation(line_location, y, scan_number)
 		}
 	}
 
@@ -119,47 +181,34 @@ func z_triangulation(x int, y int, scan_number int) float64 {
 	return z * (-float64(f))
 }
 
-func scan_image_by_dev(filename string, scan_number int, z_array [][]float64) {
+func scan_image_by_dev(filename string, scan_number int, z_array [][]float64, cal_array [][]int) {
 	fmt.Println("Opening file " + filename + ". scan number " + strconv.Itoa(scan_number))
 
 	file, err := os.Open(filename)
 	if err != nil { panic(err) }
 	r := bufio.NewReader(file)
-	scan_image, err := png.Decode(r)
+	scan, err := png.Decode(r)
 
 	start_time := time.Now()
 
 	first_line_location := -1
 	for y := 0; y < image_size_y; y += sub_sampling_rate {
-		enter_white, exit_white := 0, 0
-
-		for x := 0; x < image_size_x; x++ {
-			color := scan_image.At(x, y)
-			r, g, b, _ := color.RGBA()
-			if is_white(r, g, b) {
-				enter_white = x
-				break
-			}
-		}
-
-		for x := enter_white; x < image_size_x; x++ {
-			color := scan_image.At(x, y)
-			r, g, b, _ := color.RGBA()
-			if !is_white(r, g, b) {
-				exit_white = x - 1
-				break
-			}
-		}
-
-		if enter_white != 0 && exit_white != 0 {
-			mid_white := int((enter_white + exit_white) / 2)
-
-			if first_line_location == -1 {
-				z_array[mid_white][y] = 0
-				first_line_location = mid_white
+		line_location := find_projection_in_line(scan, y)
+		if line_location != -99999 {
+			if cal_array != nil {
+				expected_location := cal_array[scan_number][y]
+				if expected_location != -99999 {
+					xdiff := line_location - expected_location
+					z_array[line_location][y] = float64(xdiff)
+				}
 			} else {
-				x_diff := mid_white - first_line_location
-				z_array[mid_white][y] = float64(x_diff)
+				if first_line_location == -1 {
+					z_array[line_location][y] = float64(0)
+					first_line_location = line_location
+				} else {
+					xdiff := line_location - first_line_location
+					z_array[line_location][y] = float64(xdiff)
+				}
 			}
 		}
 	}
@@ -244,9 +293,9 @@ func main() {
 		fixed_dir := fix_dir_name(*dir_name)
 		if *scan {
 			if *cal_dir != "" {
-				// TODO : call scan_dir with calibration directory
+				scan_dir(fixed_dir, *cal_dir)
 			} else {
-				scan_dir(fixed_dir)
+				scan_dir(fixed_dir, "")
 			}
 		}
 	}
